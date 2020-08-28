5291e5ef378ebfbcde253ecaf0e5d653d88166a5
Zachary del Rosario <zdelrosario@outlook.com>
2020-08-28 09:57:15 -0400

    write docstring

--- SIR/apply.m
+++ SIR/apply.m
@@ -1,6 +1,20 @@
 function tab_output = apply(fun, tab_input)
+% apply Apply a function to all columns of a table
+%
+% This is a simple wrapper for the Matlab built-in function `varfun` that
+% strips the added prefix to column names.
+%
+% Usage
+%   tab_output = apply(fun, tab_input)
+% 
+% Arguments
+%   fun = function to apply to each column
+%   tab_input = table to operate upon
+%
+% Returns
+%   tab_output = modified table
 
 tab_output = varfun(fun, tab_input);
-tab_output.Properties.VariableNames = regexprep(tab_output.Properties.VariableNames, 'Fun_', '');
+tab_output.Properties.VariableNames = regexprep(tab_output.Properties.VariableNames, '^\w+_', '');
 
 end

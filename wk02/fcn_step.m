157b3d23c5d8eb974142f99fa2c2db3cf00e9f42
Zachary del Rosario <zdelrosario@outlook.com>
2020-08-28 12:47:10 -0400

    create first notebook for Wk 2: stock + flow, SIR

---
+++ wk02/fcn_step.m
@@ -0,0 +1,30 @@
+function tab_state = fcn_step(tab_state, tab_system)
+% fcn_step Advance an SIR model one timestep
+%
+% Usage
+%   tab_state = fcn_step(tab_state, tab_system)
+% 
+% Arguments
+%   tab_state = current state of SIR system, but have [s, i, r] attributes
+%   tab_system = system parameters, must have [beta, gamma] attributes
+%                beta = infection rate
+%                gamma = recovery rate
+% 
+% Returns
+%   tab_state = updated state
+
+% compute new infections and recoveries
+infected = tab_system.beta * tab_state.i * tab_state.s;
+recovered = tab_system.gamma * tab_state.i;
+    
+% Update state
+tab_state.s = tab_state.s - infected;
+tab_state.i = tab_state.i + infected - recovered;
+tab_state.r = tab_state.r + recovered;
+
+% Enforce invariants; necessary since we're doing a discrete approx.
+tab_state.s = max(tab_state.s, 0);
+tab_state.i = max(tab_state.i, 0);
+tab_state.r = max(tab_state.r, 0);
+    
+end
